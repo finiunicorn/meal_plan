8e530ef164bf062a08ef6cdc4c94f80860803288
fini <jsabew_spam@icloud.com>
2026-04-08 19:53:41 +0200

    added more meals

--- generate_meal_plan.py
+++ generate_meal_plan.py
@@ -2,18 +2,18 @@ import random
 import json
 
 meals = [
-    "Kartoffelpüree mit Erbsen, Möhren und Ei", "Risi Bisi", "Tacos", "Shakshuka", "Nudeln mit Soße",
+    "Kartoffelpüree mit Erbsen, Möhren und Ei", "Risi Bisi", "Tacos", "Nudeln mit Soße",
     "Pizza", "Erbsensuppe", "Burger", "Bulgurauflauf", "Linsensuppe", "Tomatenreis", "Curry mit Reis/Couscous",
     "Bretonische Bohnensuppe", "Oliven-Feta-Knödel", "Mie-Nudeln mit Gemüse", "Kartoffelsalat", "Gurkensuppe",
     "Gazpacho", "Bärlauch/Basilikum Klöße mit Tomatensoße", "Haloumni Omelett", "Ofengemüse", "Kartoffelgratin",
-    "Pellkartoffeln mit Quark"
+    "Pellkartoffeln mit Quark", "Möhren Kartoffeln Untereinander (Eintopf)", "Japchae"
 ]
 
 days = ["Samstag", "Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"]
 
 selected_meals = random.sample(meals, 7)
 
-# Combine into a list of dictionaries
+# combine into a list of dictionaries
 meal_plan = [{"Tag": day, "Gericht": meal} for day, meal in zip(days, selected_meals)]
 
 with open("meal_plan.json", "w") as f:
